ba0034208f089b12e9517f052f1ca0e356ec0d2b
pupii29 <67531765+pupii29@users.noreply.github.com>
2022-05-08 21:23:22 +0700

    Add files via upload

---
+++ Model.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Model extends JPanel implements ActionListener {
+    private Dimension d;
+    private final Font smallFont = new Font("Arial", Font.BOLD, 14);
+    private boolean inGame = false;
+    private boolean dying = false;
+
+    private final int PACMAN_SPEED = 6;
+    private int[] dx, dy;
+
+    private Image up, down, left, right;
+
+    private int pacman_x, pacman_y, pacmand_x, pacmand_y;
+    private int req_dx, req_dy;
+
+    private final int validSpeeds[] = {1,2,3,4,6,8};
+    private final int maxSpeed = 6;
+    private int currentSpeed = 3;
+
+    public Model(){
+        loadImages();
+    }
+
+    private void loadImages(){
+        down = new ImageIcon("/src/images/down.gif").getImage();
+        up = new ImageIcon("/src/images/up.gif").getImage();
+        left = new ImageIcon("/src/images/left.gif").getImage();
+        right = new ImageIcon("/src/images/right.gif").getImage();
+    }
+
+    class TAdapter extends KeyAdapter{
+        public void keyPressed(KeyEvent e){
+            int key = e.getKeyCode();
+
+            if (inGame){
+                if (key == KeyEvent.VK_LEFT){
+                    req_dx = -1;
+                    req_dy = 0;
+                }
+                else if(key == KeyEvent.VK_RIGHT){
+                    req_dx = 1;
+                    req_dy = 0;
+                }
+                else if(key == KeyEvent.VK_UP){
+                    req_dx = 0;
+                    req_dy = -1;
+                }else if(key == KeyEvent.VK_DOWN){
+                    req_dx = 0;
+                    req_dy = 1;
+                }
+            }
+        }
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
