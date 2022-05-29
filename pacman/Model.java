448a89639bc1c2036985961966fbc8f3f25c4aee
nguyencan285 <91868096+nguyencan285@users.noreply.github.com>
2022-05-29 16:57:38 +0700

    Main

--- pacman/Model.java
+++ pacman/Model.java
@@ -55,30 +55,24 @@ public class Model extends JPanel implements ActionListener {
     private final int maxSpeed = 6;
 
     private int currentSpeed = 3;
-    public void increaseSpeed(int currentSpeed) {
-        this.currentSpeed = currentSpeed + 1;
-    }
+
     private short[] screenData;
     private Timer timer;
 
-    public Model(){
-        loadImages();
-        addKeyListener(new TAdapter());
-        setFocusable(true);
-        initVariables();
-        initGame();
+
+
+
+
+    //setup
+    private void showIntroScreen(Graphics2D g2d) {
+        /*String hi = "Hello, welcome to our team game !";
+        g2d.setColor(Color.yellow);
+        g2d.drawString(hi, (SCREEN_SIZE)/6, 100);
+         */
+        String start = "Press SPACE to start";
+        g2d.setColor(Color.yellow);
+        g2d.drawString(start, (SCREEN_SIZE)/4, 150);
     }
-
-    private void loadImages(){
-        down = new ImageIcon("/src/images/down.gif").getImage();
-        up = new ImageIcon("/src/images/up.gif").getImage();
-        left = new ImageIcon("/src/images/left.gif").getImage();
-        right = new ImageIcon("/src/images/right.gif").getImage();
-        ghost=new ImageIcon("/src/images/ghost.gif").getImage();
-        heart=new ImageIcon("/src/images/heart.gif").getImage();
-    }
-
-
     private void drawScore(Graphics2D g){
         g.setFont(smallFont);
         g.setColor(new Color(10,200,90));
@@ -88,21 +82,126 @@ public class Model extends JPanel implements ActionListener {
             g.drawImage(heart,i*28+8,SCREEN_SIZE+1,this);
         }
     }
+    private void drawMaze(Graphics2D g2d) {
 
-    private void initVariables() {
+        short i = 0;
+        int x, y;
 
-        screenData = new short[N_BLOCKS * N_BLOCKS];
-        d = new Dimension(400, 400);
-        ghost_x = new int[MAX_GHOSTS];
-        ghost_dx = new int[MAX_GHOSTS];
-        ghost_y = new int[MAX_GHOSTS];
-        ghost_dy = new int[MAX_GHOSTS];
-        ghostSpeed = new int[MAX_GHOSTS];
-        dx = new int[4];
-        dy = new int[4];
+        for (y = 0; y < SCREEN_SIZE; y += BLOCK_SIZE) {
+            for (x = 0; x < SCREEN_SIZE; x += BLOCK_SIZE) {
 
-        timer = new Timer(40, this);
-        timer.start();
+                g2d.setColor(new Color(0,72,251));
+                g2d.setStroke(new BasicStroke(5));
+
+                if ((levelData[i] == 0)) {
+                    g2d.fillRect(x, y, BLOCK_SIZE, BLOCK_SIZE);
+                }
+
+                if ((screenData[i] & 1) != 0) {
+                    g2d.drawLine(x, y, x, y + BLOCK_SIZE - 1);
+                }
+
+                if ((screenData[i] & 2) != 0) {
+                    g2d.drawLine(x, y, x + BLOCK_SIZE - 1, y);
+                }
+
+                if ((screenData[i] & 4) != 0) {
+                    g2d.drawLine(x + BLOCK_SIZE - 1, y, x + BLOCK_SIZE - 1,
+                            y + BLOCK_SIZE - 1);
+                }
+
+                if ((screenData[i] & 8) != 0) {
+                    g2d.drawLine(x, y + BLOCK_SIZE - 1, x + BLOCK_SIZE - 1,
+                            y + BLOCK_SIZE - 1);
+                }
+
+                if ((screenData[i] & 16) != 0) {
+                    g2d.setColor(new Color(255,255,255));
+                    g2d.fillOval(x + 10, y + 10, 6, 6);
+                }
+
+                i++;
+            }
+        }
+    }
+    private void loadImages(){
+        down = new ImageIcon("/src/images/down.gif").getImage();
+        up = new ImageIcon("/src/images/up.gif").getImage();
+        left = new ImageIcon("/src/images/left.gif").getImage();
+        right = new ImageIcon("/src/images/right.gif").getImage();
+        ghost=new ImageIcon("/src/images/ghost.gif").getImage();
+        heart=new ImageIcon("/src/images/heart.gif").getImage();
+    }
+    private void drawGhost(Graphics2D g2d, int x, int y) {
+        g2d.drawImage(ghost, x, y, this);
+    }
+    private void drawPacman(Graphics2D g2d) {
+
+        if (req_dx == -1) {
+            g2d.drawImage(left, pacman_x + 1, pacman_y + 1, this);
+        } else if (req_dx == 1) {
+            g2d.drawImage(right, pacman_x + 1, pacman_y + 1, this);
+        } else if (req_dy == -1) {
+            g2d.drawImage(up, pacman_x + 1, pacman_y + 1, this);
+        } else {
+            g2d.drawImage(down, pacman_x + 1, pacman_y + 1, this);
+        }
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.black);
+        g2d.fillRect(0, 0, d.width, d.height);
+
+        drawScore(g2d);
+
+        if (inGame) {
+            playGame(g2d);
+        } else {
+            showIntroScreen(g2d);
+        }
+
+        Toolkit.getDefaultToolkit().sync();
+        g2d.dispose();
+    }
+
+    //gamelogic
+    private void movePacman() {
+
+        int pos;
+        short ch;
+
+        if (pacman_x % BLOCK_SIZE == 0 && pacman_y % BLOCK_SIZE == 0) {
+            pos = pacman_x / BLOCK_SIZE + N_BLOCKS * (int) (pacman_y / BLOCK_SIZE);
+            ch = screenData[pos];
+
+            if ((ch & 16) != 0) {
+                screenData[pos] = (short) (ch & 15);
+                score++;
+            }
+            // Check for moving
+            if (req_dx != 0 || req_dy != 0) {
+                if (!((req_dx == -1 && req_dy == 0 && (ch & 1) != 0)
+                        || (req_dx == 1 && req_dy == 0 && (ch & 4) != 0)
+                        || (req_dx == 0 && req_dy == -1 && (ch & 2) != 0)
+                        || (req_dx == 0 && req_dy == 1 && (ch & 8) != 0))) {
+                    pacmand_x = req_dx;
+                    pacmand_y = req_dy;
+                }
+            }
+            // Check for standstill
+            if ((pacmand_x == -1 && pacmand_y == 0 && (ch & 1) != 0)
+                    || (pacmand_x == 1 && pacmand_y == 0 && (ch & 4) != 0)
+                    || (pacmand_x == 0 && pacmand_y == -1 && (ch & 2) != 0)
+                    || (pacmand_x == 0 && pacmand_y == 1 && (ch & 8) != 0)) {
+                pacmand_x = 0;
+                pacmand_y = 0;
+            }
+        }
+        pacman_x = pacman_x + PACMAN_SPEED * pacmand_x;
+        pacman_y = pacman_y + PACMAN_SPEED * pacmand_y;
     }
     private void moveGhosts(Graphics2D g2d) {
         int pos;
@@ -173,8 +272,12 @@ public class Model extends JPanel implements ActionListener {
             }
         }
     }
-    private void drawGhost(Graphics2D g2d, int x, int y) {
-        g2d.drawImage(ghost, x, y, this);
+    private void death() {
+        lives--;
+        if (lives == 0) {
+            inGame = false;
+        }
+        continueLevel();
     }
     private void checkMaze() {
         int i=0;
@@ -209,15 +312,22 @@ public class Model extends JPanel implements ActionListener {
             checkMaze();
         }
     }
-    private void showIntroScreen(Graphics2D g2d) {
-        /*String hi = "Hello, welcome to our team game !";
-        g2d.setColor(Color.yellow);
-        g2d.drawString(hi, (SCREEN_SIZE)/4, 200);
 
-         */
-        String start = "Press SPACE to start";
-        g2d.setColor(Color.yellow);
-        g2d.drawString(start, (SCREEN_SIZE)/4, 150);
+    //initgame
+    private void initVariables() {
+
+        screenData = new short[N_BLOCKS * N_BLOCKS];
+        d = new Dimension(400, 400);
+        ghost_x = new int[MAX_GHOSTS];
+        ghost_dx = new int[MAX_GHOSTS];
+        ghost_y = new int[MAX_GHOSTS];
+        ghost_dy = new int[MAX_GHOSTS];
+        ghostSpeed = new int[MAX_GHOSTS];
+        dx = new int[4];
+        dy = new int[4];
+
+        timer = new Timer(40, this);
+        timer.start();
     }
     private void continueLevel() {
 
@@ -248,63 +358,6 @@ public class Model extends JPanel implements ActionListener {
         req_dy = 0;
         dying = false;
     }
-    private void death() {
-        lives--;
-        if (lives == 0) {
-            inGame = false;
-        }
-        continueLevel();
-    }
-
-    private void movePacman() {
-
-        int pos;
-        short ch;
-
-        if (pacman_x % BLOCK_SIZE == 0 && pacman_y % BLOCK_SIZE == 0) {
-            pos = pacman_x / BLOCK_SIZE + N_BLOCKS * (int) (pacman_y / BLOCK_SIZE);
-            ch = screenData[pos];
-
-            if ((ch & 16) != 0) {
-                screenData[pos] = (short) (ch & 15);
-                score++;
-            }
-            // Check for moving
-            if (req_dx != 0 || req_dy != 0) {
-                if (!((req_dx == -1 && req_dy == 0 && (ch & 1) != 0)
-                        || (req_dx == 1 && req_dy == 0 && (ch & 4) != 0)
-                        || (req_dx == 0 && req_dy == -1 && (ch & 2) != 0)
-                        || (req_dx == 0 && req_dy == 1 && (ch & 8) != 0))) {
-                    pacmand_x = req_dx;
-                    pacmand_y = req_dy;
-                }
-            }
-            // Check for standstill
-            if ((pacmand_x == -1 && pacmand_y == 0 && (ch & 1) != 0)
-                    || (pacmand_x == 1 && pacmand_y == 0 && (ch & 4) != 0)
-                    || (pacmand_x == 0 && pacmand_y == -1 && (ch & 2) != 0)
-                    || (pacmand_x == 0 && pacmand_y == 1 && (ch & 8) != 0)) {
-                pacmand_x = 0;
-                pacmand_y = 0;
-            }
-        }
-        pacman_x = pacman_x + PACMAN_SPEED * pacmand_x;
-        pacman_y = pacman_y + PACMAN_SPEED * pacmand_y;
-    }
-
-     private void drawPacman(Graphics2D g2d) {
-
-        if (req_dx == -1) {
-        	g2d.drawImage(left, pacman_x + 1, pacman_y + 1, this);
-        } else if (req_dx == 1) {
-        	g2d.drawImage(right, pacman_x + 1, pacman_y + 1, this);
-        } else if (req_dy == -1) {
-        	g2d.drawImage(up, pacman_x + 1, pacman_y + 1, this);
-        } else {
-        	g2d.drawImage(down, pacman_x + 1, pacman_y + 1, this);
-        }
-    }
-
     private void initGame() {
         lives =3;
         score =0;
@@ -320,25 +373,12 @@ public class Model extends JPanel implements ActionListener {
         }
         continueLevel();
     }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        Graphics2D g2d = (Graphics2D) g;
-
-        g2d.setColor(Color.black);
-        g2d.fillRect(0, 0, d.width, d.height);
-
-        drawScore(g2d);
-
-        if (inGame) {
-            playGame(g2d);
-        } else {
-             showIntroScreen(g2d);
-        }
-
-        Toolkit.getDefaultToolkit().sync();
-        g2d.dispose();
+    public Model(){
+        loadImages();
+        addKeyListener(new TAdapter());
+        setFocusable(true);
+        initVariables();
+        initGame();
     }
 
     class TAdapter extends KeyAdapter {
